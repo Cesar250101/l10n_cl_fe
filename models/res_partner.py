95784c410a30d078c2f4b85e6eabfe55f1bf58cb
Daniel Santibáñez Polanco <dansanti@gmail.com>
2023-03-16 06:13:05 -0300

    [FIX]Dependencia faltante
    close #118

--- models/res_partner.py
+++ models/res_partner.py
@@ -4,6 +4,7 @@ import logging
 from odoo import api, fields, models, tools
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
+import re
 
 _logger = logging.getLogger(__name__)
 try:
