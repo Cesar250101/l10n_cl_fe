4575a43d9d8b16ffeab84792f01df055770595f4
Daniel Santibáñez Polanco <dansanti@gmail.com>
2023-09-15 15:05:08 -0300

    [FIX]Compute name modificando propuesta odoo

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": """Facturación Electrónica para Chile\
     """,
-    "version": "0.37.0",
+    "version": "0.37.1",
     "category": "Localization/Chile",
     "sequence": 12,
     "author": "Daniel Santibáñez Polanco, Cooperativa OdooCoop",
