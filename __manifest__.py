fbcd69cad7d54f90ffb5ff772d446fd16db53eca
Daniel Santibáñez Polanco <dansanti@gmail.com>
2023-09-17 14:45:09 -0300

    bump version

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": """Facturación Electrónica para Chile\
     """,
-    "version": "0.38.1",
+    "version": "0.38.2",
     "category": "Localization/Chile",
     "sequence": 12,
     "author": "Daniel Santibáñez Polanco, Cooperativa OdooCoop",
