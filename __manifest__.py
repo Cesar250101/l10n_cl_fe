9b3f834b3eb6f5f54d8ecb5b442f0a7ef266198f
Daniel Santibáñez Polanco <dansanti@gmail.com>
2023-11-18 19:52:48 -0300

    [IMP]mejora en manejo de folios

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": """Facturación Electrónica para Chile\
     """,
-    "version": "0.39.3",
+    "version": "0.39.4",
     "category": "Localization/Chile",
     "sequence": 12,
     "author": "Daniel Santibáñez Polanco, Cooperativa OdooCoop",
